9cca468a689a741f2098611eed3324dbc637c989
HOME <HOME@HOME-PC>
2021-02-17 23:03:51 +0530

    First Logical Programs to git (First Commit)

---
+++ Logical_Programs/src/com/prg/swapno/SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+package com.prg.swapno;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int num1 =10 , num2=20 , num3 =30; 
+		
+		int temp = 0;
+		System.out.println("Numbers before swaping : num1 : "+num1 +" num2 : "+num2);
+		temp = num1;
+		num1=num2;
+		num2=temp;
+		System.out.println("Numbers before swaping : num1 : "+num1 +" num2 : "+num2);
+	}
+
+}
